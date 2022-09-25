6aea52a9c2ff9942ec9ef83b86eaee80f0f35f20
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2022-09-25 21:26:46 +0700

    Adding handler and helper go

--- Assignment_2/handler/customer.go
+++ Assignment_2/handler/customer.go
@@ -1,2 +1,40 @@
 package handler 
 
+import (
+	"assignment_2/customers"
+	"assignment_2/helper"
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+type customerHandler struct {
+	service customers.Service
+}
+
+func NewCustomerHandler(service customers.Service) *customerHandler {
+	return &customerHandler{service}
+}
+
+func (h *customerHandler) CreateCustomer(c *gin.Context) {
+	var input customers.CustomerInput
+
+	err := c.ShouldBindJSON(&input)
+
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Failed creating customer", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+	}
+
+	newCustomer, err := h.service.CreateCustomer(input)
+	if err != nil {
+		response := helper.APIResponse("Failed to create customer", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Success to create customer", http.StatusOK, "success", customers.FormatCustomer(newCustomer))
+	c.JSON(http.StatusOK, response)
+}
---
+++ Assignment_2/handler/order.go
@@ -0,0 +1,142 @@
+package handler 
+
+import (
+	"assignment_2/orders"
+	"assignment_2/helper"
+	"net/http"
+	"strconv"
+	"github.com/gin-gonic/gin"
+)
+
+type orderHandler struct {
+	service orders.Service
+}
+
+func NewOrderHandler(service orders.Service) *orderHandler {
+	return &orderHandler{service}
+}
+
+func (h *orderHandler) CreateOrder(c *gin.Context) {
+	var input orders.OrderInput
+
+	err := c.ShouldBindJSON(&input)
+
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Failed creating order", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	newOrder, err := h.service.Create(input)
+	if err != nil {
+		response := helper.APIResponse("Failed to create order, customer id not found", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Success to create order", http.StatusOK, "success", orders.FormatOrder(newOrder))
+	c.JSON(http.StatusOK, response)
+}
+
+func (h *orderHandler) GetOrders(c *gin.Context) {
+	order, err := h.service.GetOrders()
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Failed getting all orders", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	response := helper.APIResponse("Orders", http.StatusOK, "success", orders.FormatOrders(order))
+	c.JSON(http.StatusOK, response)
+}
+
+func (h *orderHandler) UpdateOrder(c *gin.Context) {
+	var inputID orders.GetOrderByID
+
+	err := c.ShouldBindUri(&inputID)
+	if err != nil {
+		response := helper.APIResponse("Failed updating order", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	var inputData orders.OrderUpdate
+
+	err = c.ShouldBindJSON(&inputData)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Failed updating order", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	updatedOrder, err := h.service.UpdateOrder(inputID, inputData)
+	if err != nil {
+		response := helper.APIResponse("Failed updating order", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Success updating order", http.StatusOK, "success", orders.FormatOrder(updatedOrder))
+	c.JSON(http.StatusOK, response)
+}
+
+func (h *orderHandler) GetOrderByID(c *gin.Context) {
+	var inputID orders.GetOrderByID
+
+	err := c.ShouldBindUri("id")
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Failed getting order", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	order, err := h.service.GetOrderByID(inputID)
+	if err != nil {
+		response := helper.APIResponse("Failed getting order", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Success getting order", http.StatusOK, "success", orders.FormatOrder(order))
+	c.JSON(http.StatusOK, response)
+}
+
+func (h *orderHandler) DeleteOrder(c *gin.Context) {
+
+	idparam := c.Param("id")
+	id, err := strconv.Atoi(idparam)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Failed deleting order", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	order, err := h.service.DeleteOrder(id)
+	if err != nil {
+		response := helper.APIResponse("Failed deleting order", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Success deleting Order", http.StatusOK, "success", orders.FormatOrder(order))
+	c.JSON(http.StatusOK, response)
+}
+
+
+
+
